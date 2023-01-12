557fab432beebbc65b7e01f9aaad4fb0bc44bdab
DenzelWashington <denzelwashingtonsantana@gmail.com>
2023-01-12 16:29:22 -0300

    Finish verifyToken and add authentication route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,12 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { deleteUser, getAllUsers, getUser, updateUser } from "../controllers/UserController";
+import { verifyToken } from "../utils/verifyToken";
 
 const router = express.Router()
+
+router.get('/checkauthentication', verifyToken, (req: Request,res: Response,next: NextFunction) => {
+    res.send('Olá usuário, você está autenticado :)')
+})
 
 //UPDATE
 router.put('/:id', updateUser)
--- src/utils/verifyToken.ts
+++ src/utils/verifyToken.ts
@@ -1,15 +1,28 @@
 import { NextFunction, Request, Response } from 'express'
-import jwt from 'jsonwebtoken'
+import jwt, { JwtPayload, VerifyErrors } from 'jsonwebtoken'
 
-export const verifyToken = (req: Request, res: Response, next: NextFunction): void => {
-  const token = req.cookies.access_token
-  if(!token) {
-    next(res.status(404).json('Você não está autenticado'))
+export interface CustomRequest extends Request {
+  token: string | JwtPayload
+}
+
+export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const token = req.cookies.access_token
+    if(!token) {
+      return next(res.status(404).json('Você não está autenticado'))
+    }
+    
+    jwt.verify(token, process.env.JWT as string, (err: VerifyErrors | null, user: any) => {
+      if(err) {
+        return next(res.status(404).json('Token não válido!'))
+      }
+      (req as CustomRequest).token = user
+      next()
+    }) 
   }
-
-  jwt.verify(token, process.env.JWT as string, (err ,user) => {
-  if(err) return next(res.status(403).json('Token não válido!'))
-  req.user = user
-  next()
-  })
+  
+  catch(err) {
+    res.status(401).send('Por favor autentique-se')
+  }
+  
 }
